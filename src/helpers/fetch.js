61be98d8ba5b5ab32200af77cf74b0e40683879d
Jorge Tordecilla <jtordeci@ibero.edu.co>
2022-04-02 00:20:05 -0500

    deploy config

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:4000";
+const baseUrl = "https://actividad3server.herokuapp.com/";
 
 export const fetchApi = (endpoint, data, method = "GET") => {
   const url = `${baseUrl}/${endpoint}`;
